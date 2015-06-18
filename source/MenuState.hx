57122bf2e26fcba7e9c9ef362c028d6f2696d772
denjin <luffmcgruff@hotmail.com>
2015-06-18 16:19:37 +0100

    Preliminary Work
    Setup basic menu and play states, added FlxTilemap and highlight
    graphic.

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -7,16 +7,32 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 
+import flixel.util.FlxDestroyUtil;
+
+using flixel.util.FlxSpriteUtil;
+
+
 /**
  * A FlxState which can be used for the game's menu.
  */
-class MenuState extends FlxState
-{
+class MenuState extends FlxState {
+	private var menuTitle:FlxText;
+	private var playButton:FlxButton;
+	
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
-	override public function create():Void
-	{
+	override public function create():Void {
+		//create text for the top of the menu
+		menuTitle = new FlxText(0, 0, 0, "PixelCastles", 14);
+		add(menuTitle);
+		
+		//create button to play game
+		playButton = new FlxButton(0, 0, "Play", clickPlay);
+		playButton.screenCenter();
+		add(playButton);
+		
+		
 		super.create();
 	}
 	
@@ -24,16 +40,21 @@ class MenuState extends FlxState
 	 * Function that is called when this state is destroyed - you might want to 
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
-	override public function destroy():Void
-	{
+	override public function destroy():Void {
 		super.destroy();
+		menuTitle = FlxDestroyUtil.destroy(menuTitle);
+		playButton = FlxDestroyUtil.destroy(playButton);
+		
 	}
 
 	/**
 	 * Function that is called once every frame.
 	 */
-	override public function update():Void
-	{
+	override public function update():Void {
 		super.update();
-	}	
+	}
+	
+	private function clickPlay():Void {
+		FlxG.switchState(new PlayState());
+	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,22 +1,44 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
+import openfl.Assets;
+import flixel.util.FlxSpriteUtil;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
+import flixel.util.FlxColor;
+
+import flixel.util.FlxDestroyUtil;
+
+import flixel.tile.FlxTilemap;
 
 /**
  * A FlxState which can be used for the actual gameplay.
  */
-class PlayState extends FlxState
-{
+class PlayState extends FlxState {
+	//the size of the tilemap tiles
+	private static inline var TILE_WIDTH:Int = 16;
+	private static inline var TILE_HEIGHT:Int = 16;
+	
+	//the tilemap
+	private var terrainMap:FlxTilemap;
+	
+	private var highlightBox:FlxSprite;
+	
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
-	override public function create():Void
-	{
+	override public function create():Void {
+		terrainMap = new FlxTilemap();
+		
+		highlightBox = new FlxSprite(0, 0);
+		highlightBox.makeGraphic(TILE_WIDTH, TILE_HEIGHT, FlxColor.TRANSPARENT);
+		FlxSpriteUtil.drawRect(highlightBox, 0, 0, TILE_WIDTH - 1, TILE_HEIGHT - 1, FlxColor.TRANSPARENT, { thickness:1, color:FlxColor.WHITE } );
+		add(highlightBox);
+		
 		super.create();
 	}
 	
@@ -24,16 +46,18 @@ class PlayState extends FlxState
 	 * Function that is called when this state is destroyed - you might want to 
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
-	override public function destroy():Void
-	{
+	override public function destroy():Void {
+		FlxDestroyUtil.destroy(terrainMap);
 		super.destroy();
 	}
 
 	/**
 	 * Function that is called once every frame.
 	 */
-	override public function update():Void
-	{
+	override public function update():Void {
+		highlightBox.x = Math.floor(FlxG.mouse.x / TILE_WIDTH) * TILE_WIDTH;
+		highlightBox.y = Math.floor(FlxG.mouse.y / TILE_HEIGHT) * TILE_HEIGHT;
+		
 		super.update();
-	}	
+	}
 }
